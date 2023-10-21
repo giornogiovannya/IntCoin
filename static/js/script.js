991b613cbbd97650315423eadaefd5b6f8b47f59
MaxSkr0210 <maxskr0210@mail.ru>
2023-10-21 12:28:19 +0300

    fix carousel

--- static/js/script.js
+++ static/js/script.js
@@ -1,72 +1,47 @@
-const carousel = document.querySelectorAll(".carousel");
+const carousel = document.querySelectorAll(".slider-container");
 
 for(let car of carousel){
-  const firstBlock = car.querySelectorAll(".goods_item")[0];
+  let innerSlider = car.querySelector('.inner-slider');
+  
+  let pressed = false;
+  let startX;
+  let x;
 
-  const category = car.classList[1]
+  car.addEventListener('mousedown', (e) => {
+    pressed = true;
+    startX = e.offsetX - innerSlider.offsetLeft;
+    car.style.cursor = "grabbing";
+  })
   
-  let isDragStart = false;
-  let isDragging = false
-  let prevPageX;
-  let prevScrollLeft;
-  let positionDiff;
+  car.addEventListener("mouseenter", () => {
+    car.style.cursor = "grab";
+  });
   
-  function outList(goods) {
-    const list = car;
+  car.addEventListener("mouseup", () => {
+    car.style.cursor = "grab";
+    pressed = false;
+  });
   
-    let html = "";
+  car.addEventListener("mousemove", (e) => {
+    if (!pressed) return;
+    
+    e.preventDefault();
+    x = e.offsetX;
+    innerSlider.style.left = `${x - startX}px`;
+    checkBoundary();
+
+  });
   
-    for (let goodsItem of goods) {
-      html += `<div class="goods_item">
-                        <p>${goodsItem.name}</p>
-                        <p>${goodsItem.description}</p>
-                        <p>${goodsItem.price}</p>
-                      </div>`;
+  const checkBoundary = () => {
+    let outer = car.getBoundingClientRect();
+    let inner = innerSlider.getBoundingClientRect();
+
+    if (parseInt(innerSlider.style.left) > 0) {
+        innerSlider.style.left = "0px";
     }
-    list.innerHTML = html;
-  }
-  
-  async function start() {
-    const res = (await axios.get(`${host}/goods?filter=type&value=${category}`)).data;
-    outList(res);
-  }
-  
-  window.addEventListener("load", start);
-  
-  const autoSlide = () => {
-      if(car.scrollLeft - (car.scrollWidth - car.clientWidth) > -1 || car.scrollLeft <= 0) return;
-      positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
-      let firstImgWidth = firstBlock.clientWidth + 14;
-      let valDifference = firstImgWidth - positionDiff;
-      if(car.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
-          return car.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-      }
-      car.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-  }
-  const dragStart = (e) => {
-      isDragStart = true;
-      prevPageX = e.pageX || e.touches[0].pageX;
-      prevScrollLeft = car.scrollLeft;
-  }
-  const dragging = (e) => {
-      if(!isDragStart) return;
-      e.preventDefault();
-      isDragging = true;
-      car.classList.add("dragging");
-      positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-      car.scrollLeft = prevScrollLeft - positionDiff;
-  }
-  const dragStop = () => {
-      isDragStart = false;
-      car.classList.remove("dragging");
-      if(!isDragging) return;
-      isDragging = false;
-      autoSlide();
-  }
-  car.addEventListener("mousedown", dragStart);
-  car.addEventListener("touchstart", dragStart);
-  document.addEventListener("mousemove", dragging);
-  car.addEventListener("touchmove", dragging);
-  document.addEventListener("mouseup", dragStop);
-  car.addEventListener("touchend", dragStop);
+
+    if (inner.right < outer.right) {
+        innerSlider.style.left = `-${inner.width - outer.width}px`;
+    }
+  };
 }
