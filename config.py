cd828e3f972fdde8a7e11e448a23d7f144c25261
giornogiovannya <teklik7@gmail.com>
2023-10-21 19:44:14 +0300

    upd structure, config, cleaned goods_bot

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-bot_token = "6768981325:AAFoWSL7UEoCgcPfTch-jZQDHta1Sr4gqDY"
+goods_bot_token = "6768981325:AAFoWSL7UEoCgcPfTch-jZQDHta1Sr4gqDY"
+admin_bot_token = "6777401801:AAEo-Q2M5N3LnGWce0hn3FsI__t7oEzna2g"
+admin_user_id = 1643016272
--- goods_bot.py
+++ goods_bot.py
@@ -3,13 +3,13 @@ import json
 import logging
 import sys
 from aiogram import Bot, Dispatcher, types
-from config import bot_token
+from config import goods_bot_token
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=bot_token)
+bot = Bot(token=goods_bot_token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
@@ -27,20 +27,6 @@ async def cmd_web(message: types.Message):
     await bot.send_message(chat_id=message.chat.id,
                            text="Да пожалуйста:",
                            reply_markup=keyboard)
-
-
-# Под рассылку
-@dp.message_handler(commands=['sendall'])
-async def send_all(message: types.Message):
-    admin_id = 1643016272
-    if message.chat.id == 1643016272:
-        await message.answer("Старт")
-        users = []
-        for i in users:
-            await bot.send_message(chat_id=i, text="Текст")
-        await message.answer('Рассылка окончена')
-    else:
-        await message.answer('Ошибка авторизации')
 
 
 if __name__ == "__main__":
